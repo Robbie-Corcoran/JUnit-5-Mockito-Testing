d5c2c7fcb973409dd8e21ecec27d81d5cb9cc923
Robbie Corcoran <codewithrobbie@gmail.com>
2023-10-18 12:18:30 +0200

    Methods Order - Random order

---
+++ src/test/java/MethodsOrderedRandomlyTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.Random.class)
+public class MethodsOrderedRandomlyTest {
+
+    @Test
+    void testA(TestInfo testInfo) {
+        System.out.println("Running " +testInfo.getTestMethod().get().getName());
+    }
+
+    @Test
+    void testB(TestInfo testInfo) {
+        System.out.println("Running " +testInfo.getTestMethod().get().getName());
+    }
+
+    @Test
+    void testC(TestInfo testInfo) {
+        System.out.println("Running " +testInfo.getTestMethod().get().getName());
+    }
+
+    @Test
+    void testD(TestInfo testInfo) {
+        System.out.println("Running " +testInfo.getTestMethod().get().getName());
+    }
+}
