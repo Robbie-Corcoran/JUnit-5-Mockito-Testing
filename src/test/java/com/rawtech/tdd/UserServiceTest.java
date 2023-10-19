47c3b27bdba704f0038da2efb172fee5848222be
Robbie Corcoran <codewithrobbie@gmail.com>
2023-10-19 14:05:01 +0200

    TDD - Refactor Test method

--- src/test/java/com/rawtech/tdd/UserServiceTest.java
+++ src/test/java/com/rawtech/tdd/UserServiceTest.java
@@ -1,14 +1,17 @@
 package com.rawtech.tdd;
 
 import com.rawtech.tdd.model.User;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserServiceTest {
 
+    @DisplayName("User object created and name correct.")
     @Test
-    void testCreateUser_whenUserDetailsProvided_returnUserObject() {
+    void testCreateUser_whenUserDetailsProvided_returnUserObjectAndFirstName() {
 //        Arrange
         UserService userService = new UserServiceImpl();
 
@@ -23,23 +26,9 @@ public class UserServiceTest {
 
 //        Assert
         assertNotNull(user, "createUser() should have returned a User object");
+        assertEquals(firstName, user.getFirstName(), "User's'first name is not correct ");
+        assertEquals(lastName, user.getLastName(), "User's last name is not correct ");
+
     }
 
-    @Test
-    void testCreateUser_whenUserIsCreated_returnedUserObjectContainsCorrectFirstName() {
-        //        Arrange
-        UserService userService = new UserServiceImpl();
-
-        String firstName = "Robbie";
-        String lastName = "Corcoran";
-        String email = "robbie@robbie.com";
-        String password = "123456789";
-        String repeatedPassword = "123456789";
-
-//        Act
-        User user = userService.createUser(firstName, lastName, email, password, repeatedPassword);
-
-//        Assert
-        assertEquals(firstName, user.getFirstName(), "User first name is not correct ");
-    }
 }
